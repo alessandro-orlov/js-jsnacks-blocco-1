8c3ae4a5c1d89ce3b4d132c56a0e84aa3730429c
Alessandro <alex.ixs@hotmail.com>
2020-05-28 17:28:34 +0200

    JSnack 3 - completato

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,6 @@
 //========== JSnack 1 =================
-//L’utente inserisce due numeri in successione, con due prompt.
-//Il software stampa il maggiore.
+// L’utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore.
 
 // alert('JSnack - Esercizio 1')
 //
@@ -14,13 +14,13 @@
 // } while( (secondoNumero == '') || (secondoNumero == null) || (isNaN(secondoNumero)) )
 //
 // //Messagio universale
-// var risultato = `I numeri si equivalgono perché ${primoNumero} è uguale a ${secondoNumero}`
+// var risultato = 'I numeri si equivalgono perché ' + primoNumero + ' è uguale a ' + secondoNumero;
 //
 // //LOGICA
 // if(primoNumero > secondoNumero) {
-//   risultato = `il primo numero ${primoNumero} è maggiore del secondo`;
+//   risultato = 'il primo numero ' + primoNumero + ' è maggiore del secondo';
 // } else if (primoNumero < secondoNumero) {
-//   risultato = `il secondo numero ${secondoNumero} è maggiore del primo`;
+//   risultato = 'il secondo numero ' + secondoNumero + ' è maggiore del primo';
 // }
 //
 // //Output
@@ -33,30 +33,32 @@
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
-//Chiedo di inserire due parole all0utente
-do {
-  var primaParola = prompt('Inserisci la prima parola');
-} while ( (primaParola == null) || (primaParola == '') );
+// alert('JSnack - Esercizio 2')
 
-do {
-  var secondaParola = prompt('Inserisci la seconda parola');
-} while ( (secondaParola == null) || (secondaParola == '') );
-
-//Lungheza delle parole
-var lunghezzaPrimaParola = primaParola.length;
-var lunghezzaSecondaParola = secondaParola.length;
-
-//LOGICA
-var messaggio = 'La parole hanno la stessa lungheza ';
-//Comparazione delle lunghezze delle parole
-if( lunghezzaPrimaParola > lunghezzaSecondaParola) {
-  messaggio = secondaParola + ' ' + primaParola;
-} else if (lunghezzaPrimaParola < lunghezzaSecondaParola ) {
-  messaggio = primaParola + ' ' + secondaParola;
-}
-
-//OUTPUT
-alert(messaggio)
+// //Chiedo di inserire due parole all0utente
+// do {
+//   var primaParola = prompt('Inserisci la prima parola');
+// } while ( (primaParola == null) || (primaParola == '') );
+//
+// do {
+//   var secondaParola = prompt('Inserisci la seconda parola');
+// } while ( (secondaParola == null) || (secondaParola == '') );
+//
+// //Lungheza delle parole
+// var lunghezzaPrimaParola = primaParola.length;
+// var lunghezzaSecondaParola = secondaParola.length;
+//
+// //LOGICA
+// var messaggio = 'La parole hanno la stessa lungheza ';
+// //Comparazione delle lunghezze delle parole
+// if( lunghezzaPrimaParola > lunghezzaSecondaParola) {
+//   messaggio = secondaParola + ' ' + primaParola;
+// } else if (lunghezzaPrimaParola < lunghezzaSecondaParola ) {
+//   messaggio = primaParola + ' ' + secondaParola;
+// }
+//
+// //OUTPUT
+// alert(messaggio)
 
 
 
@@ -65,6 +67,41 @@ alert(messaggio)
 // Il software deve chiedere per 5 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma in due versioni, con il for e con il while.
 
+// alert('JSnack - Esercizio 3 (Versione - for)')
+//
+// var somma = 0;
+//
+// for(var i = 1; i <=5; i++) {
+//   do {
+//     var insertNumeber = parseInt(prompt('Inserisci un numero'));
+//   } while ( isNaN(insertNumeber) == true );
+//   console.log('hai inserito numero ' + insertNumeber)
+//
+//   //Calcolo la somma
+//   somma = somma + insertNumeber;
+// }
+//
+// alert('La somma dei numeri inseriti è: ' + somma);
+
+
+alert('JSnack - Esercizio 3 (Versione - while)')
+var somma = 0;
+
+var i = 1;
+while(i <= 5) {
+  do {
+    var numero = parseInt(prompt('inserisci numero'));
+      } while (isNaN(numero) == true);
+
+  console.log('hai inserito il numero: ' + numero);
+
+  somma = somma + numero;
+
+  //Interruzione del loop
+  i++
+}
+console.log('somma dei numeri inseriti è ' + somma)
+alert('La somma dei numeri inseriti ' + somma);
 
 
 
